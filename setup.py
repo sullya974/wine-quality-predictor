2d48f40fedc11d217eb97e059af08b59052e52e4
sullya974 <sully.adelard@laposte.net>
2020-02-28 12:03:51 +0100

    setup project dependencies

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='wqp',
+    version='1.0.0',
+    author='webmestre.gerard@gmail.com',
+    description='Wine quality predictor - a package machine learning algo to predict wine quality'
+    packages=setuptools.find-packages(),
+    install_requires=[
+        "scikit-learn==0.22.1",
+        "pandas==1.0.1"
+    ]
+)        
